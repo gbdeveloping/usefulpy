d63e9cc098c91d769a49ab8a08a67879c1493748
Gaspar B. Development <70714917+gbdvt@users.noreply.github.com>
2021-03-01 18:34:49 +0100

    Delete FaceCriminalASCIIChar.py

--- FaceCriminalASCIIChar.py
+++
@@ -1,8 +0,0 @@
-hair, cheek, eye, nose, mouth, chin = input().split()
-
-print(hair * 5)
-print(cheek + eye + ' ' + eye + cheek)
-print(cheek + ' ' + nose + ' ' + cheek)
-print(cheek + ' ' + mouth + ' ' + cheek)
-x = int((6 - len(chin))/2)
-print( ' ' * x + chin)
